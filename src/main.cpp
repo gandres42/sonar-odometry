c50ef79ef97404efe28c13eb806cf60d52043e0d
gandres42 <gandres2416@gmail.com>
2025-10-06 00:56:09 +0000

    code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdint-uintn.h>
+#include <open3d/utility/Eigen.h>
 #include <sstream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
@@ -6,7 +8,11 @@
 #include <cv_bridge/cv_bridge.h>
 #include <open3d/Open3D.h>
 #include <Eigen/Dense>
+#include "FusionMath.h"
+#include "geometry_msgs/Pose.h"
 #include "ros/ros.h"
+#include "ros/subscriber.h"
+#include "ros/time.h"
 #include "std_msgs/String.h"
 #include "sensor_msgs/Image.h"
 #include "sensor_msgs/Imu.h"
@@ -15,23 +21,36 @@
 #include "kalman.hpp"
 #include "Fusion.h"
 #include "FusionAhrs.h"
+#include "nav_msgs/Odometry.h"
 
 #define SONAR_MAX_DIST 5
-#define SONAR_BUFFER_LENGTH 2
+#define SONAR_BUFFER_LENGTH 3
 #define USE_COMPASS true
 #define COMPASS_HEADING 196
 #define USE_ARUCO false
 
-struct stampedFrame {
+using namespace Eigen;
+
+struct sonarFrame {
   cv_bridge::CvImagePtr cv_ptr;
   uint64_t time_ns;
 };
 
-struct stampedPosition {
+struct magFrame {
+  FusionVector mag;
+  uint64_t time_ns;
+};
+
+struct arucoFrame {
   double x;
   double y;
   double z;
   double yaw;
+  double yaw_deg;
+  uint64_t time_ns;
+};
+
+struct imuFrame {
   uint64_t time_ns;
 };
 
@@ -59,53 +78,52 @@ private:
     cv::KAZE::DIFF_PM_G1
   );
   std::unique_ptr<cv::BFMatcher> bf = std::make_unique<cv::BFMatcher>(cv::NORM_HAMMING);
-  std::deque<stampedFrame> frame_ptr_buffer;
+  std::deque<sonarFrame> frame_ptr_buffer;
 
   // rotation fusion filter
   FusionAhrs ahrs;
-  uint64_t prev_imu_time;
-  FusionVector mag_vector;
-  uint64_t prev_mag_time;
+
+  // message buffers
+  magFrame mag_frame;
+  arucoFrame aruco_frame;
+  sonarFrame sonar_frame;
+  imuFrame imu_frame;
 
   // position kalman filter
   int n;
   int m;
   KalmanFilter filter;
-  Eigen::MatrixXd C;
-  Eigen::MatrixXd C_positionless;
-  Eigen::MatrixXd A;
+  MatrixXd C;
+  MatrixXd C_positionless;
+  MatrixXd A;
 
-  // aruco tracking
-  stampedPosition aruco_pose;
 
 public:
   SonarOdom() {
-    // subscribe to sonar image
     sonar_sub = node.subscribe<sensor_msgs::Image>("/oculus/drawn_sonar", 1, &SonarOdom::sonar_cb, this);
-    imu_sub = node.subscribe<sensor_msgs::Imu>("/mavros/imu/data", 1, &SonarOdom::imu_cb, this);
+    imu_sub = node.subscribe<sensor_msgs::Imu>("/mavros/imu/data_raw", 1, &SonarOdom::imu_cb, this);
     mag_sub = node.subscribe<sensor_msgs::MagneticField>("/mavros/imu/mag", 1, &SonarOdom::mag_cb, this);
-    pose_sub = node.subscribe<geometry_msgs::PoseStamped>("/aruco/pose", 1, &SonarOdom::pose_cb, this);
-    pose_pub = node.advertise<geometry_msgs::PoseStamped>("/sianat/pose", 1);
+    pose_sub = node.subscribe<geometry_msgs::PoseStamped>("/aruco/pose", 1, &SonarOdom::aruco_cb, this);
+
+    pose_pub = node.advertise<nav_msgs::Odometry>("/sianat/pose", 1);
   
-    // aruco tracking setup
-    aruco_pose = {0, 0, 0, 0, ros::Time::now().toNSec()};
+    // message buffers
+    uint64_t one_second_ago = ros::Time::now().toNSec() - uint64_t(1e9);
+    aruco_frame = {0, 0, 0, 0, 0, one_second_ago};
+    sonar_frame = {nullptr, one_second_ago};
+    imu_frame = {one_second_ago};
+    
 
     // fusion filter construction
     FusionAhrsInitialise(&ahrs);
-    prev_imu_time = ros::Time::now().toNSec();
-    mag_vector = {0, 0, 0};
-    prev_mag_time = std::numeric_limits<uint64_t>::max();
 
     // kalman filter construction
-    double dt = 1.0/10.0;
-
-    A = Eigen::MatrixXd(n, n);
-
-    // Remove linear acceleration as a state: n = 6, m = 6
     n = 6; // Number of states (x, y, z, vx, vy, vz)
     m = 6; // Number of measurements (x, y, z, vx, vy, vz)
+    
+    A = MatrixXd(n, n);
 
-    C = (Eigen::MatrixXd(m, n) <<
+    C = (MatrixXd(m, n) <<
       1, 0, 0, 0, 0, 0,
       0, 1, 0, 0, 0, 0,
       0, 0, 1, 0, 0, 0,
@@ -114,7 +132,7 @@ public:
       0, 0, 0, 0, 0, 1
     ).finished(); // measurement mapping matrix with position
 
-    C_positionless = (Eigen::MatrixXd(m, n) <<
+    C_positionless = (MatrixXd(m, n) <<
       0, 0, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 0,
@@ -123,35 +141,37 @@ public:
       0, 0, 0, 0, 0, 1
     ).finished(); // measurement mapping matrix without position
 
-    Eigen::MatrixXd Q = Eigen::MatrixXd::Identity(n, n) * .01; // Process noise covariance
-    Eigen::MatrixXd R = Eigen::MatrixXd::Identity(m, m) * .02; // Measurement noise covariance
-    Eigen::MatrixXd P = (Eigen::MatrixXd(n, n) <<
+    MatrixXd Q = MatrixXd::Identity(n, n) * .01; // Process noise covariance
+    MatrixXd R = MatrixXd::Identity(m, m) * .02; // Measurement noise covariance
+    MatrixXd P = (MatrixXd(n, n) <<
       1, 0, 0,  0,  0,  0,
       0, 1, 0,  0,  0,  0,
       0, 0, 1,  0,  0,  0,
-      0, 0, 0, .2,  0,  0,
-      0, 0, 0,  0, .2,  0,
-      0, 0, 0,  0,  0, .2
+      0, 0, 0, .4,  0,  0,
+      0, 0, 0,  0, .4,  0,
+      0, 0, 0,  0,  0, .4
     ).finished(); // Estimate error covariance
 
-    // initialize filter with no position updates
-    filter = KalmanFilter(dt, A, C_positionless, Q, R, P);
+    filter = KalmanFilter(0.1, A, C_positionless, Q, R, P);
     filter.init();
   }
 
-  void pose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg) {
-    this->aruco_pose = {
-      msg->pose.position.x,
+  void aruco_cb(const geometry_msgs::PoseStamped::ConstPtr &msg) {
+    this->aruco_frame = {
+      msg->pose.position.x, 
       msg->pose.position.y,
       msg->pose.position.z,
       msg->pose.orientation.z,
+      msg->pose.orientation.z * 180.0f / M_PI,
       ros::Time::now().toNSec()
     };
   }
 
   void mag_cb(const sensor_msgs::MagneticField::ConstPtr& msg) {
-    this->prev_mag_time = ros::Time::now().toNSec();
-    this->mag_vector = {float(msg->magnetic_field.x), float(msg->magnetic_field.y), float(msg->magnetic_field.z)};
+    this->mag_frame = {
+      {float(msg->magnetic_field.x), float(msg->magnetic_field.y), float(msg->magnetic_field.z)},
+      ros::Time::now().toNSec()
+    };
   }
 
   void imu_cb(const sensor_msgs::Imu::ConstPtr& msg) {
@@ -165,62 +185,54 @@ public:
       float(msg->linear_acceleration.y / 9.80665),
       float(msg->linear_acceleration.z / 9.80665)
     };
-    float dt = (ros::Time::now().toNSec() - this->prev_imu_time) / 1e9;
+    float imu_dt = (ros::Time::now().toNSec() - this->imu_frame.time_ns) / 1e9;
+    float mag_dt = (ros::Time::now().toNSec() - this->mag_frame.time_ns) / 1e9;
+    float aruco_dt = (ros::Time::now().toNSec() - this->aruco_frame.time_ns) / 1e9;
 
     // update IMU with either magnetometer or aruco as external heading
     if (USE_COMPASS) {
-      bool mag_fresh = ros::Time::now().toNSec() - 2e8 > this->prev_mag_time;
-      if (mag_fresh) {
-        FusionAhrsUpdateNoMagnetometer(&this->ahrs, gyro, accel, dt);
+      if (mag_dt <= 0.1) {
+        FusionAhrsUpdate(&this->ahrs, gyro, accel, this->mag_frame.mag, imu_dt);
       }
       else {
-        FusionAhrsUpdate(&this->ahrs, gyro, accel, this->mag_vector, dt);
+        FusionAhrsUpdateNoMagnetometer(&this->ahrs, gyro, accel, imu_dt);
       }
     }
     else {
-      bool pose_fresh = ((ros::Time::now().toNSec() - this->aruco_pose.time_ns) / 1e9) < 0.15;
-      if (pose_fresh) {
-        float aruco_yaw_deg = this->aruco_pose.yaw * 180.0f / M_PI;
-        FusionAhrsUpdateExternalHeading(&this->ahrs, gyro, accel, aruco_yaw_deg, dt);
+      if (aruco_dt <= 0.1) {
+        FusionAhrsUpdateExternalHeading(&this->ahrs, gyro, accel, this->aruco_frame.yaw_deg, imu_dt);
       }
       else {
-        FusionAhrsUpdateNoMagnetometer(&this->ahrs, gyro, accel, dt);
+        FusionAhrsUpdateNoMagnetometer(&this->ahrs, gyro, accel, imu_dt);
       }
     }
-    this->prev_imu_time = ros::Time::now().toNSec();
+    this->imu_frame = {ros::Time::now().toNSec()};
 
-    // publish pose with madgwick and kalman filter states
-    geometry_msgs::PoseStamped pose_msg;
+    // publish pose using imu filter and raw acceleration
+    nav_msgs::Odometry pose_msg;
     pose_msg.header.stamp = ros::Time::now();
     pose_msg.header.frame_id = "map_ned";
     pose_msg.pose.pose.position.x = this->filter.state().transpose()[0];
     pose_msg.pose.pose.position.y = this->filter.state().transpose()[1];
     pose_msg.pose.pose.position.z = this->filter.state().transpose()[2];
-    pose_msg.twist.twist.linear.x = this->filter.state().transpose()[3];
-    pose_msg.twist.twist.linear.y = this->filter.state().transpose()[4];
-    pose_msg.twist.twist.linear.z = this->filter.state().transpose()[5];
+    pose_msg.twist.twist.linear.x = msg->angular_velocity.x;
+    pose_msg.twist.twist.linear.y = msg->angular_velocity.y;
+    pose_msg.twist.twist.linear.z = msg->angular_velocity.z;
 
-    // rotate pose by compass offset if needed
+    // get rotation and adjust by compass offset if needed
+    FusionQuaternion quat = FusionAhrsGetQuaternion(&this->ahrs);
     if (USE_COMPASS) {
-      FusionQuaternion quat = FusionAhrsGetQuaternion(&this->ahrs);
-      Eigen::AngleAxisf compass_rot(float(360 - COMPASS_HEADING) * M_PI / 180.0f, Eigen::Vector3f::UnitZ());
-      Eigen::Quaternionf q_rotated = compass_rot * Eigen::Quaternionf(quat.array[0], quat.array[1], quat.array[2], quat.array[3]);
+      AngleAxisf compass_rot(float(360 - COMPASS_HEADING) * M_PI / 180.0f, Vector3f::UnitZ());
+      Quaternionf q_rotated = compass_rot * Quaternionf(quat.array[0], quat.array[1], quat.array[2], quat.array[3]);
       quat.array[0] = q_rotated.w();
       quat.array[1] = q_rotated.x();
       quat.array[2] = q_rotated.y();
       quat.array[3] = q_rotated.z();
-      pose_msg.pose.orientation.w = quat.array[0];
-      pose_msg.pose.orientation.x = quat.array[1];
-      pose_msg.pose.orientation.y = quat.array[2];
-      pose_msg.pose.orientation.z = quat.array[3];
     }
-    else {
-      FusionQuaternion quat = FusionAhrsGetQuaternion(&this->ahrs);
-      pose_msg.pose.pose.orientation.w = quat.array[0];
-      pose_msg.pose.pose.orientation.x = quat.array[1];
-      pose_msg.pose.pose.orientation.y = quat.array[2];
-      pose_msg.pose.pose.orientation.z = quat.array[3];
-    }
+    pose_msg.pose.pose.orientation.w = quat.array[0];
+    pose_msg.pose.pose.orientation.x = quat.array[1];
+    pose_msg.pose.pose.orientation.y = quat.array[2];
+    pose_msg.pose.pose.orientation.z = quat.array[3];
 
     this->pose_pub.publish(pose_msg);
   }
@@ -260,20 +272,9 @@ public:
     cv::Mat des_b;
     this->akaze->detectAndCompute(prev_image->image, cv::noArray(), kp_b, des_b);
 
-    // use BFMatcher to find best matches
-    std::vector<std::vector<cv::DMatch>> prelim_matches;
+    // use BFMatcher to find matches
     std::vector<std::vector<cv::DMatch>> matches;
-    this->bf->knnMatch(des_a, des_b, prelim_matches, 2);
-
-    // filter out only the good matches from BFMatcher
-    for (int i = 0; i < prelim_matches.size(); i++) {
-      cv::DMatch* m = &prelim_matches[i][0];
-      cv::DMatch* n = &prelim_matches[i][1];
-
-      if (m->distance < (0.25 * n->distance)) {
-        matches.push_back(prelim_matches[i]);
-      }    
-    }
+    this->bf->knnMatch(des_a, des_b, matches, 2);
 
     // perform RANSAC to eliminate bad matches
     std::vector<cv::Point2f> pts_a, pts_b;
@@ -281,9 +282,9 @@ public:
       pts_a.push_back(kp_a[match_pair[0].queryIdx].pt);
       pts_b.push_back(kp_b[match_pair[0].trainIdx].pt);
     }
-    if (pts_a.size() >= 8 && pts_b.size() >= 8) {
+    if (pts_a.size() >= 5 && pts_b.size() >= 5) {
       std::vector<uchar> inliers_mask;
-      cv::findHomography(pts_a, pts_b, cv::RHO, 1, inliers_mask, 4000, 0.99);
+      cv::findHomography(pts_a, pts_b, cv::RHO, 1, inliers_mask, 4000, 0.999);
 
       std::vector<std::vector<cv::DMatch>> inlier_matches;
       for (size_t i = 0; i < inliers_mask.size(); ++i) {
@@ -295,7 +296,7 @@ public:
     }
 
     // matches visualization
-    if (this->visualize && !matches.empty()) {
+    if (this->visualize) {
       cv::Mat img_matches;
       std::vector<cv::DMatch> flat_matches;
       for (const auto& m : matches) {
@@ -326,8 +327,8 @@ public:
         (kp_b[matches[i][0].trainIdx].pt.y) / 100
       });
     }
-    Eigen::MatrixXf matched_A(2, matched_pts_a.size());
-    Eigen::MatrixXf matched_B(2, matched_pts_b.size());
+    MatrixXf matched_A(2, matched_pts_a.size());
+    MatrixXf matched_B(2, matched_pts_b.size());
     for (size_t i = 0; i < matched_pts_a.size(); ++i) {
       matched_A(0, i) = matched_pts_a[i][0];
       matched_A(1, i) = matched_pts_a[i][1];
@@ -337,58 +338,51 @@ public:
 
     // get current ahrs rotation and extract only rotation about z
     FusionQuaternion quat = FusionAhrsGetQuaternion(&this->ahrs);
-    Eigen::Quaternionf q;
+    Quaternionf q;
     if (USE_COMPASS) {
-      Eigen::AngleAxisf compass_rot(float(360 - COMPASS_HEADING) * M_PI / 180.0f, Eigen::Vector3f::UnitZ());
-      q = compass_rot * Eigen::Quaternionf(quat.array[0], quat.array[1], quat.array[2], quat.array[3]);
+      AngleAxisf compass_rot(float(360 - COMPASS_HEADING) * M_PI / 180.0f, Vector3f::UnitZ());
+      q = compass_rot * Quaternionf(quat.array[0], quat.array[1], quat.array[2], quat.array[3]);
     }
     else {
-      q = Eigen::Quaternionf(quat.array[0], quat.array[1], quat.array[2], quat.array[3]);
+      q = Quaternionf(quat.array[0], quat.array[1], quat.array[2], quat.array[3]);
     }
-    Eigen::Matrix3f rot3d = q.toRotationMatrix();
+    Matrix3f rot3d = q.toRotationMatrix();
 
     // rotate translation to match global z rotation
-    auto euler = rot3d.eulerAngles(0, 1, 2);
-    float yaw = euler[2];
-    Eigen::Matrix2f rot2d;
-    rot2d << cos(yaw), -sin(yaw), sin(yaw), cos(yaw);
-
+    float yaw = rot3d.eulerAngles(0, 1, 2)[2];
+    Matrix2f rot2d = (Matrix2f() << cos(yaw), -sin(yaw), sin(yaw), cos(yaw)).finished();
     matched_A = rot2d * matched_A;
     matched_B = rot2d * matched_B;
 
     // SVD with default of no translation in degenerate cases
-    Eigen::Vector2f t = (Eigen::Vector2f() << 0, 0).finished();
+    Vector2f t = (Vector2f() << 0, 0).finished();
     if (matched_A.cols() > 4 || matched_B.cols() > 4) {
-      Eigen::Vector2f centroid_A = matched_A.rowwise().mean();
-      Eigen::Vector2f centroid_B = matched_B.rowwise().mean();
-      Eigen::MatrixXf AA = matched_A.colwise() - centroid_A;
-      Eigen::MatrixXf BB = matched_B.colwise() - centroid_B;
-      Eigen::Matrix2f H = AA * BB.transpose();
+      Vector2f centroid_A = matched_A.rowwise().mean();
+      Vector2f centroid_B = matched_B.rowwise().mean();
+      MatrixXf AA = matched_A.colwise() - centroid_A;
+      MatrixXf BB = matched_B.colwise() - centroid_B;
+      Matrix2f H = AA * BB.transpose();
       if (!H.isZero(1e-6)) {
-        Eigen::JacobiSVD<Eigen::Matrix2f> svd(H, Eigen::ComputeFullU | Eigen::ComputeFullV);
-        Eigen::Matrix2f R = svd.matrixV() * svd.matrixU().transpose();
+        JacobiSVD<Matrix2f> svd(H, ComputeFullU | ComputeFullV);
+        Matrix2f R = svd.matrixV() * svd.matrixU().transpose();
         if (R.determinant() < 0) {
-          Eigen::Matrix2f V = svd.matrixV();
+          Matrix2f V = svd.matrixV();
           V.col(1) *= -1;
           R = V * svd.matrixU().transpose();
         }
         t = centroid_B - R * centroid_A;
       }
     }
-
-    // t = rot2d * t;
-
     double dt = (msg->header.stamp.toNSec() - prev_time) / 1e9;
     float vx = t[1] / dt;
     float vy = -t[0] / dt;
 
     // get acceleration
     FusionVector accel = FusionAhrsGetLinearAcceleration(&this->ahrs);
-    Eigen::Vector3f accel_vec = (Eigen::Vector3f() << accel.axis.x, accel.axis.y, accel.axis.z).finished();
-    accel_vec = rot3d * accel_vec;
+    Vector3f accel_vec = (Vector3f() << accel.axis.x, accel.axis.y, accel.axis.z).finished();
 
     // State transition matrix A for 6-state model (x, y, z, vx, vy, vz)
-    A = (Eigen::MatrixXd(n, n) <<
+    A = (MatrixXd(n, n) <<
       1, 0, 0, dt, 0,  0,
       0, 1, 0, 0,  dt, 0,
       0, 0, 1, 0,  0,  dt,
@@ -397,21 +391,21 @@ public:
       0, 0, 0, 0,  0,  1
     ).finished();
 
-    std::cout << "dt: " << dt << ", dx: " << dx << ", dy: " << dy << std::endl;
+    std::cout << "dt: " << dt << ", dx: " << vx << ", dy: " << vy << std::endl;
     float vz = 0.0f;
 
     // check if position is up to date, if true update filter with position
-    float pose_dt = (ros::Time::now().toNSec() - this->aruco_pose.time_ns) / 1e9;
+    float pose_dt = (ros::Time::now().toNSec() - this->aruco_frame.time_ns) / 1e9;
     // pose_dt = 0.2;
     if (pose_dt < 0.1) {
       std::cout << "using position" << std::endl;
-      Eigen::VectorXd sensor_meas = (Eigen::VectorXd(n) << this->aruco_pose.x, this->aruco_pose.y, this->aruco_pose.z, vx, vy, 0).finished();
+      VectorXd sensor_meas = (VectorXd(m) << this->aruco_frame.x, this->aruco_frame.y, this->aruco_frame.z, vx, vy, 0).finished();
       this->filter.C = this->C;
       this->filter.update(sensor_meas, dt, A);
     }
     else {
       std::cout << "not using position" << std::endl;
-      Eigen::VectorXd sensor_meas = (Eigen::VectorXd(n) << 0, 0, 0, vx, vy, 0).finished();
+      VectorXd sensor_meas = (VectorXd(m) << 0, 0, 0, vx, vy, 0).finished();
       this->filter.C = this->C_positionless;
       this->filter.update(sensor_meas, dt, A);
     }
